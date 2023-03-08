a136a60c17bac11fbbd9e4df1fbc6c8325626899
AshS <emailash62@gmail.com>
2023-03-08 11:44:02 +0800

    add db.js. build out apiRoutes to add a note to JSON file.

--- Develop/routes/apiRoutes.js
+++ Develop/routes/apiRoutes.js
@@ -4,8 +4,30 @@ const router = express.Router();
 // random id
 const uuid = require("uuid");
 
+const DB = require("../db/DB");
+
 // get notes
 router.get("/api/notes", async function (req, res) {
     const notes = await DB.readNotes();
     return res.json(notes);
   });
+
+  // add a new note to json file
+router.post("/api/notes", async function (req, res) {
+    const currentNotes = await DB.readNotes();
+    let newNote = {
+      id: uuid(),
+      title: req.body.title,
+      text: req.body.text,
+    };
+  
+    await DB.addNote([...currentNotes, newNote]);
+  
+    return res.send(newNote);
+  });
+
+
+
+
+
+  module.exports = router;
